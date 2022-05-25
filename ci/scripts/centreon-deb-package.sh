dd2ccd8bab6b8f76fb83a25c61bfd1f99bae8a89
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2022-05-25 15:10:03 +0200

    test

--- ci/scripts/centreon-deb-package.sh
+++ ci/scripts/centreon-deb-package.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+export CYPRESS_CACHE_FOLDER"=/var/cache/cypress"
 
 if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
   echo "You need to specify VERSION / RELEASE / DISTRIB variables"
